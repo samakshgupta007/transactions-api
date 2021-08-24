2de8cee6ff96babe1f8c34899c75cdc465438232
Samaksh Gupta <samaksh@tsbmediaventure.com>
2021-08-24 14:28:47 +0530

    fixing indentation

--- src/db/dal/transaction.ts
+++ src/db/dal/transaction.ts
@@ -18,7 +18,11 @@ export const getMerchantDataByUserId = async ({ userId, from, to } : UserIdDto) 
         error
     };
 
-    const userTransactions = await sequelizeConnection.query(`SELECT * from (SELECT "Transaction"."user_id", PERCENT_RANK() OVER(PARTITION BY merchant_id ORDER BY SUM(amount) ASC) AS "Percentile", "merchants"."display_name" FROM "transactions" AS "Transaction" LEFT OUTER JOIN "merchants" ON "Transaction"."merchant_id" = "merchants"."id" WHERE "Transaction"."date" BETWEEN '${moment(from).format('YYYY-MM-DD HH:mm:ss')}' AND '${moment(to).format('YYYY-MM-DD HH:mm:ss')}' GROUP BY "Transaction"."user_id", "Transaction"."merchant_id", "merchants"."id", "merchants"."display_name") AS SUBQUERY WHERE user_id = ${userId};`,
+    const userTransactions = await sequelizeConnection.query(
+            `SELECT * from (SELECT "Transaction"."user_id", PERCENT_RANK() OVER(PARTITION BY merchant_id ORDER BY SUM(amount) ASC) AS "Percentile", "merchants"."display_name" 
+            FROM "transactions" AS "Transaction" LEFT OUTER JOIN "merchants" ON "Transaction"."merchant_id" = "merchants"."id" WHERE "Transaction"."date" BETWEEN 
+            '${moment(from).format('YYYY-MM-DD HH:mm:ss')}' AND '${moment(to).format('YYYY-MM-DD HH:mm:ss')}' GROUP BY "Transaction"."user_id", "Transaction"."merchant_id", 
+            "merchants"."id", "merchants"."display_name") AS SUBQUERY WHERE user_id = ${userId};`,
     { type: QueryTypes.SELECT })
     .then(function(results) {
         return results;
